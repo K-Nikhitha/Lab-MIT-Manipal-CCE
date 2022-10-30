40a8f56c639facb4488447efc45c59b4e34dd8a5
Nikhitha <75841155+K-Nikhitha@users.noreply.github.com>
2022-10-30 17:43:04 +0530

    Create BoolExecute.java

---
+++ Lab2/BoolExecute.java
@@ -0,0 +1,9 @@
+public class BoolExecute {
+    public static void main(String[] args) {
+    boolean x= true;
+    //int y=x;
+    //System.out.println(y);
+    int y= (int)x;
+    System.out.println(y);
+}
+}
