dc147ecb38b3acf29a902c805bc2d75c0c25e7e1
Nikhitha <75841155+K-Nikhitha@users.noreply.github.com>
2022-10-30 17:36:45 +0530

    Create BoolExecute.java

---
+++ ObjectOrientedProgramming/BoolExecute.java
@@ -0,0 +1,11 @@
+public class BoolExecute 
+{
+    public static void main(String[] args) 
+    {
+        boolean x= true;
+        //int y=x;
+        //System.out.println(y);
+        int y= (int)x;
+        System.out.println(y);
+    }
+}
